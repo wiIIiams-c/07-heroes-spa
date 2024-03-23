1424cc8b15ce73649a9b2b8948288ca0148793ce
Williams C <wilcontrerasf@gmail.com>
2024-03-23 14:31:03 -0300

    Update app
    * se separa entre rutas publicas y privadas creando sus router respectivos
    * se configura que en caso se vuelva al login y vuelva a entrear esta carge en la ultima pagina que estaba visualizando
    * se crean los types para el ogin y su respectivo reducer
    * se deja en sus respectivo index las exportaciones

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,10 +1,17 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context";
 
 export const LoginPage = () => {
+    const {login} = useContext(AuthContext);
     const navigate = useNavigate();
 
     const onLogin = () => {
-        navigate('/', {
+        const lastPath = localStorage.getItem('lastPath') || '/';
+
+        login('Waldo Prepucio');
+
+        navigate(lastPath, {
             replace: true
         });
     }
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,13 +1,27 @@
 import { Route, Routes } from 'react-router-dom'
 import { HeroesRoutes } from '../heroes'
 import { LoginPage } from '../auth'
+import { PrivateRoute } from './PrivateRoute'
+import { PublicRoute } from './PublicRoute'
 
 export const AppRouter = () => {
     return (
         <>
             <Routes>
-                <Route path="login" element={<LoginPage />}></Route>
-                <Route path="/*" element={<HeroesRoutes />}></Route>
+                <Route path="/login" element={
+                    <PublicRoute>
+                        <LoginPage />
+                    </PublicRoute>
+                }></Route>
+                
+                <Route path='/*' element={
+                    <PrivateRoute>
+                        <HeroesRoutes />
+                    </PrivateRoute>
+                }></Route>
+
+                {/* <Route path="login" element={<LoginPage />}></Route> */}
+                {/* <Route path="/*" element={<HeroesRoutes />}></Route> */}
             </Routes>
         </>
     )
